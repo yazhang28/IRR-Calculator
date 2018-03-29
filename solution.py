4379aa577b70e85d52e4b5042001eb6df38f0ec6
yazhang28 <yazhang@bu.edu>
2018-03-29 02:21:58 -0400

    completed irr/npv methods

--- solution.py
+++ solution.py
@@ -2,20 +2,28 @@
 """
 IRR calculator
 03/28/18
+
+references: https://docs.scipy.org/doc/numpy-1.10.4/reference/generated/numpy.irr.html
 """
 
-import numpy as np
+import numpy as np # array arithmetic
+from scipy.optimize import fsolve # for root calculation
 
 # given array of cashflow, cashflow[0] is the initial investment
 # number of periods t
 
-# npv = sum((cashflow at t/( 1 + irr) ** t)) 
-def calcNPV(irr, t, cashFlow):
+# IRR given by npv = sum((cashflow at t/( 1 + irr) ** t)) 
+def npvForIRR(irr, t, cashFlow):
     periods = np.arange(t) 
-    npv = np.sum(cashFlow / (1 + irr) ** periods)
-    return npv
+    return np.sum(cashFlow / (1 + irr) ** periods)
+       
+def calcIRR(t, cashflow, x = 0.1):
+    # calculate rate: polynomial root for which y - intercept (NPV) = 0
+    irr = fsolve(npvForIRR, args = (t, cashflow), x0=x)
+    return irr
     
-
-def calcIRR(t, cashflow):
-    # calculate rate: polynomial root
-    pass
+# actual NPV
+def npv(irr, t, cashflow):
+    periods = np.arange(1,t)
+    return np.sum(cashflow[1:]/(1 + irr) **periods[1:]) + cashflow[0]
+    
