2b5023d5a45ece677cbb7a10d65024c2bc032a52
yazhang28 <yazhang@bu.edu>
2018-03-29 12:18:54 -0400

    cleaned up irrCalculator.py

--- irrCalculator.py
+++ irrCalculator.py
@@ -39,32 +39,11 @@ def repeatFLow(initial, amount, n):
 
 # x: starting estimate for f(NPV) = 0
 def calcResults(t, cashflow, x): 
+    print('given cashflow: ', cashflow)
+    print('period t: ', t)
+    print('starting estimate value for IRR: ', x)
+    print()
     irr = calcIRR(t, cashflow, x)
     print('IRR: %.2f%%' % (irr*100))
     validate(irr, t, cashflow)
     print()
-        
-
-'''
-# Tests #
-
-# given array of cashflow (cs), number of periods (t) starting from 0 
-# cs[0] is the initial investment (negative value)
-
-# cash inflow
-cs = np.array([-70000,12000,15000,18000,21000,26000])
-t = 6
-calcResults(t, cs, 0.1)
-
-# fixed cash inflow
-t2, cs2 = repeatFLow(-100.0, 60.0, 3)
-calcResults(t2, cs2, 0.1)
-
-cs3 = np.array([-100, 100, 0, 7])
-t3 = 4
-calcResults(t3, cs3, 0.1)
-
-cs4 = np.array([-100, 0, 0, 74])
-t4 = 4
-calcResults(t4, cs4, 0.1)
-'''
